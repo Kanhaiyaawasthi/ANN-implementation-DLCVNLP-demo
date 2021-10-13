daa3c75af4ee5f6693e4090d3e7df8e5644cf1f2
Kanhaiyaawasthi <kanhaiyaawasthi96@gmail.com>
2021-10-14 01:01:57 +0530

    yaml

---
+++ src/training.py
@@ -0,0 +1,16 @@
+from src.utils.common import read_config
+import argparse
+
+def training(config_path):
+    config =read_config(config_path)
+
+    print(config)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    args.add_argument("--config","-c", default="config.yaml")
+
+    parsed_args =args.parse_args()
+
+    training(config_path =parsed_args.comfig)
