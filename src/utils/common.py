ff73d802f7835628584ba441acd24f461e7be62a
Kanhaiyaawasthi <kanhaiyaawasthi96@gmail.com>
2021-10-14 03:00:16 +0530

    m

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,7 +1,7 @@
-import yaml
+import yml
 
 def read_config(config_path):
     with open(config_path) as config_file:
-        content = yaml.safe_load(config_file)
+        content = yml.safe_load(config_file)
 
     return content
